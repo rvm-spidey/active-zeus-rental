b140c49a2f723c0a3cf955ac44dda424ef6bb867
rishi monohur <rishi.monohur15@gmail.com>
2023-08-23 19:04:47 +0400

    styling for listings

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,12 @@ class AdminController < ApplicationController
   def index
     @mylistings = FitnessEquipment.where(user_id: current_user.id)
 
-    @booked_equipments = FitnessEquipment.where(user_id: current_user.id)
+    @booked_equipments = get_bookings_made
+  end
+
+  private
+
+  def get_bookings_made
+    Booking.all.where.not(user_id: current_user.id).where(status: "Pending")
   end
 end
