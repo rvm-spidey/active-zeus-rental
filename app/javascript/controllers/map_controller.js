79ceff1cf938f84fa492f311c762641ee7f23967
rishi monohur <rishi.monohur15@gmail.com>
2023-08-27 19:36:47 +0400

    added map functionality and address search

---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,47 @@
+import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
+// Connects to data-controller="map"
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: String
+  }
+
+
+  connect() {
+
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
+    })
+
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }))
+  }
+
+  #addMarkersToMap() {
+    const marker =  JSON.parse(this.markersValue)
+    const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+
+    const customMarker = document.createElement("div")
+    customMarker.innerHTML = marker.marker_html
+
+    new mapboxgl.Marker(customMarker)
+      .setLngLat([ marker.lng, marker.lat])
+      .setPopup(popup)
+      .addTo(this.map)
+  }
+
+  #fitMapToMarkers() {
+    const marker =  JSON.parse(this.markersValue);
+    const bounds = new mapboxgl.LngLatBounds()
+    bounds.extend([marker.lng, marker.lat ])
+    this.map.fitBounds(bounds, { padding: 30, maxZoom: 10, duration: 1000 })
+  }
+}
