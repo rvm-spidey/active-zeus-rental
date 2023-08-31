cdcbe6252d25b85d5337fb75f983446847bad27b
Luchmee Toolseeram <luchmee.toolseeram@gmail.com>
2023-08-31 04:32:50 +0400

    correcting-and-remodelling-index-page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_26_114916) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_161323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_26_114916) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "fitness_equipments", force: :cascade do |t|
