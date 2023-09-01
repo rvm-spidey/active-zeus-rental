2b0d589e6d398c9e8b3a7edca7e1c1ece416cdb9
rishi monohur <rishi.monohur15@gmail.com>
2023-09-01 21:31:53 +0400

    fix bug

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -18,18 +18,21 @@ class BookingsController < ApplicationController
     days_diff = (@booking.date_to - @booking.date_from).to_i
     @booking.total = days_diff * @fitness_equipment.price
 
-    # @booking_exists = Booking.where(fitness_equipment_id:@fitness_equipment.id).count
-    # puts "@bbooking_exists #{@booking_exists}"
+    @booking_exists = Booking.where(fitness_equipment_id:@fitness_equipment.id).where('date_from >= ? AND date_to<= ?',@booking.date_from, @booking.date_to).count
+
+    if @booking.save
+      redirect_to bookings_path
+    end
 
     # if @booking_exists > 0
     #   @booking.errors.add(:base, "There is already a booking for this date range")
     #   render :new, status: :unprocessable_entity
-    if @booking.save
-      redirect_to bookings_path
-    else
-      @booking = Booking.new
-      render :new, status: :unprocessable_entity
-    end
+    # elsif @booking.save
+    #   redirect_to bookings_path
+    # else
+    #   @booking = Booking.new
+    #   render :new, status: :unprocessable_entity
+    # end
   end
 
   def destroy
